2edeae18437dd9747395f6199aecb4045e8ae38b
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-31 15:18:16 -0300

    organizando projeto e adicionando input

--- App.tsx
+++ App.tsx
@@ -1,29 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/screens/Home";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025</Text>
-    </View>
-  );
+export function App() {
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#fdfcfe",
-    fontSize: 24,
-    fontWeight: "700",
-    marginTop: 48,
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16,
-  },
-});
