84f937d429847e0c058655ae4b8135aff1e30d6d
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-31 12:54:55 -0300

    adicionando estilizacao no app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "700",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+});
